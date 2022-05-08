1f4883ea9a4642d85d913d039c9d50dd7f327268
Tajwar <finched9@gmail.com>
2022-05-09 05:34:14 +0600

    Added Lists of starter vowels, starter consonants, ending vowels, ending consonants; basic algorithm to add them all together;

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from random import choice
+from random import randint
 
+starter_consonants = ['L', 'C', 'J', 'P', 'D', 'B', 'M', 'El', 'Er', 'Iv']
+starter_vowels = "aeiouy"
+ending_consonants = ['sh', 'ck', 'm', 'l', 'd', 'ce', 'se']
+ending_vowels = "aeiouy"
+
+for _ in range(10):
+    print(choice(starter_consonants)+choice(starter_vowels)+choice(ending_consonants)+choice(ending_vowels))
+
+input("Press Enter to exit...")
